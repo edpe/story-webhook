ba90a66e11afe638721becb48a3824fb6f8f512d
edpe <ed@edperkins.co.uk>
2018-10-22 16:24:33 +0100

    create story schema and model

---
+++ storyModel.js
@@ -0,0 +1,14 @@
+const connection = require('./connection');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const storySchema = new Schema({
+  title: String,
+  selectedCount: Number,
+  readCount: Number,
+  ratings: [Number]
+});
+
+const storyModel = connection.model('Story', storySchema);
+
+module.exports = storyModel;
