33d0ff9e5b5f6b3e554b403003f950068b45499d
edpe <ed@edperkins.co.uk>
2018-10-25 09:59:56 +0100

    add translation map for ratings

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-"use strict";
-require("dotenv").config();
+'use strict';
+require('dotenv').config();
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express().use(bodyParser.json()); // creates http server
 const token = process.env.TOKEN; // verification token
 const port = process.env.PORT;
+const ratings = {
+  'Really Helpful': 5,
+  'It helped a bit': 4,
+  'It was ok': 3,
+  'Not much': 2,
+  'Not at all': 1
+};
 
 const sessions = {};
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   // check if verification token is correct
   if (req.query.token !== token) {
     return res.sendStatus(401);
   }
 
-  // return challenge,
   return res.end(req.query.challenge);
 });
 
-app.post("/", (req, res) => {
+app.post('/', (req, res) => {
   // check if verification token is correct
   if (req.query.token !== token) {
     return res.sendStatus(401);
@@ -27,49 +33,55 @@ app.post("/", (req, res) => {
 
   const result = req.body.result;
   const response = {
-    sessionAttributes: { save: "me" },
+    sessionAttributes: { save: 'me' },
     responses: [
       {
-        type: "text",
-        elements: ["ok", "thanks"]
+        type: 'text',
+        elements: ['']
       }
     ]
   };
 
-  if (result.interaction.name.substring(0, 12) === "choose story") {
+  if (result.interaction.name.substring(0, 12) === 'choose story') {
     switch (result.interaction.name) {
-      case "choose story 1":
-        console.log("choose story 1");
+      case 'choose story 1':
+        console.log('choose story 1');
         break;
-      case "choose story 2":
-        console.log("choose story 2");
+      case 'choose story 2':
+        console.log('choose story 2');
         break;
-      case "choose story 3":
-        console.log("choose story 3");
+      case 'choose story 3':
+        console.log('choose story 3');
         break;
     }
-  } else if (result.interaction.name.substring(0, 12) === "end of story") {
+  } else if (result.interaction.name.substring(0, 12) === 'end of story') {
     switch (result.interaction.name) {
-      case "end of story 1":
-        console.log("increment story 1 completed count");
+      case 'end of story 1':
+        console.log('increment story 1 completed count');
         break;
-      case "end of story 2":
-        console.log("increment story 2 completed count");
+      case 'end of story 2':
+        console.log('increment story 2 completed count');
         break;
-      case "end of story 3":
-        console.log("increment story 3 completed count");
+      case 'end of story 3':
+        console.log('increment story 3 completed count');
         break;
     }
-  } else if (result.interaction.name.substring(0, 10) === "rate story") {
+  } else if (result.interaction.name.substring(0, 10) === 'rate story') {
     switch (result.interaction.name) {
-      case "rate story 1":
-        console.log("add rating of " + result.resolvedQuery + " to story 1");
+      case 'rate story 1':
+        console.log(
+          'add rating of ' + ratings[result.resolvedQuery] + ' to story 1'
+        );
         break;
-      case "rate story 2":
-        console.log("add rating of " + result.resolvedQuery + " to story 2");
+      case 'rate story 2':
+        console.log(
+          'add rating of ' + ratings[result.resolvedQuery] + ' to story 2'
+        );
         break;
-      case "rate story 2":
-        console.log("add rating of " + result.resolvedQuery + " to story 3");
+      case 'rate story 3':
+        console.log(
+          'add rating of ' + ratings[result.resolvedQuery] + ' to story 3'
+        );
         break;
     }
   }
@@ -77,4 +89,4 @@ app.post("/", (req, res) => {
   return res.json(response);
 });
 
-app.listen(port, () => console.log("[BotEngine] Webhook is listening"));
+app.listen(port, () => console.log('[BotEngine] Webhook is listening'));
