d5ed5a7b04bee9e4072724feaa765cf993f96533
edpe <ed@edperkins.co.uk>
2018-10-25 14:57:03 +0100

    create Library class for db interaction with stories

---
+++ library.js
@@ -0,0 +1,53 @@
+class Library {
+  constructor(storyModel) {
+    this.storyModel = storyModel;
+  }
+
+  logStoryChoice(story) {
+    this.storyModel.findOneAndUpdate(
+      { title: story },
+      { $inc: { selectedCount: 1 } },
+      { new: true },
+      function(err, response) {
+        if (err) {
+          console.log('error', err);
+        } else {
+          console.log('response', response);
+        }
+      }
+    );
+  }
+
+  logStoryCompleted(story) {
+    this.storyModel.findOneAndUpdate(
+      { title: story },
+      { $inc: { readCount: 1 } },
+      { new: true },
+      function(err, response) {
+        if (err) {
+          console.log('error', err);
+        } else {
+          console.log('response', response);
+        }
+      }
+    );
+  }
+
+  addStoryRating(story, rating) {
+    console.log(story, rating)
+    this.storyModel.findOneAndUpdate(
+      { title: story },
+      { $push: { ratings: rating } },
+      { new: true },
+      function(err, response) {
+        if (err) {
+          console.log('error', err);
+        } else {
+          console.log('response', response);
+        }
+      }
+    );
+  }
+}
+
+module.exports = Library;
